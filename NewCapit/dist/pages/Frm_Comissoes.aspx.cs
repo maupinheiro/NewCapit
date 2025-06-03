c76c36b1d37763d66cab3bd889b1c13e52aeed92
ccepcapit <gsaprevi@gmail.com>
2025-06-03 19:25:17 -0300

    atualizacão comissões

---
+++ NewCapit/dist/pages/Frm_Comissoes.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NewCapit.dist.pages
+{
+    public partial class Frm_Comissoes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["UsuarioLogado"] != null)
+                {
+                    string nomeUsuario = Session["UsuarioLogado"].ToString();
+                    var lblUsuario = nomeUsuario;
+
+                   // txtUsuCadastro.Text = nomeUsuario;
+
+                }
+                else
+                {
+                    var lblUsuario = "<Usuário>";
+                   // txtUsuCadastro.Text = lblUsuario;
+                }
+                PreencherComboFiliais();             
+
+
+
+
+            }
+        }
+        private void PreencherComboFiliais()
+        {
+            // Consulta SQL que retorna os dados desejados
+            string query = "SELECT codigo, descricao FROM tbempresa";
+
+            // Crie uma conexão com o banco de dados
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["conexao"].ToString()))
+            {
+                try
+                {
+                    // Abra a conexão com o banco de dados
+                    conn.Open();
+
+                    // Crie o comando SQL
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    // Execute o comando e obtenha os dados em um DataReader
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Preencher o ComboBox com os dados do DataReader
+                    cbFiliais.DataSource = reader;
+                    cbFiliais.DataTextField = "descricao";  // Campo que será mostrado no ComboBox
+                    cbFiliais.DataValueField = "codigo";  // Campo que será o valor de cada item                    
+                    cbFiliais.DataBind();  // Realiza o binding dos dados                   
+                    cbFiliais.Items.Insert(0, new ListItem("", "0"));
+                    // Feche o reader
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Trate exceções
+                    Response.Write("Erro: " + ex.Message);
+                }
+            }
+        }
+    }
+}
