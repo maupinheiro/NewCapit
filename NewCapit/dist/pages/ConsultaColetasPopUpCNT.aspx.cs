eb05b9d2086329a0e0416e8604c138634fb85199
ccepcapit <gsaprevi@gmail.com>
2025-08-14 22:09:11 -0300

    consulta pop data

--- NewCapit/dist/pages/ConsultaColetasPopUpCNT.aspx.cs
+++ NewCapit/dist/pages/ConsultaColetasPopUpCNT.aspx.cs
@@ -30,7 +30,7 @@ namespace NewCapit.dist.pages
             string connStr = ConfigurationManager.ConnectionStrings["conexao"].ConnectionString;
             using (SqlConnection conn = new SqlConnection(connStr))
             {
-                string query = "SELECT id, carga, data_hora, cliorigem, clidestino, veiculo, tipo_viagem, solicitacoes,peso,pedidos, andamento FROM tbcargas WHERE andamento = 'PENDENTE'";
+                string query = "SELECT id, carga, CAST(data_hora AS datetime), 103) + ' ' + CONVERT(varchar, CAST(data_hora AS datetime), 108) AS data_hora, cliorigem, clidestino, veiculo, tipo_viagem, solicitacoes,peso,pedidos, andamento FROM tbcargas WHERE andamento = 'PENDENTE'";
                 SqlCommand command = new SqlCommand(query, conn);
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
                 DataTable dataTable = new DataTable();
