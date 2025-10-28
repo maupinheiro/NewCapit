7bf19c5d6421b1cfec2954186111f25c91ce1088
maupinheiro <mauricio.developergroup@gmail.com>
2025-10-28 19:31:58 -0300

    consulta motoristas - nucleo

--- DAL/ConMotoristas.cs
+++ DAL/ConMotoristas.cs
@@ -31,7 +31,7 @@ namespace DAL
 
         public static DataTable FetchDataTable2(string searchTerm)
         {
-            string sql = "SELECT caminhofoto, codmot, nommot, cargo, tipomot, funcao, horario, codtra, transp, fone2, nucleo, CONVERT(varchar, dtnasc, 103) AS dtnasc, cpf, CONVERT(varchar, cadmot, 103) AS cadmot, status, id FROM tbmotoristas where fl_exclusao is null and codmot LIKE @searchTerm or nommot LIKE @searchTerm  ORDER BY nommot";
+            string sql = "SELECT caminhofoto, codmot, nommot, cargo, tipomot, funcao, horario, codtra, transp, fone2, nucleo, CONVERT(varchar, dtnasc, 103) AS dtnasc, cpf, CONVERT(varchar, cadmot, 103) AS cadmot, status, id FROM tbmotoristas where fl_exclusao is null and codmot LIKE @searchTerm or nommot LIKE @searchTerm or nucleo like @searchTerm  ORDER BY nommot";
 
             using (var con = ConnectionUtil.GetConnection())
             {
