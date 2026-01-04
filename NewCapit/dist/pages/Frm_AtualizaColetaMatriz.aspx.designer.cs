6045e2ce94eed2804bf32936443917bd1ed21dad
maupinheiro <mauricio.developergroup@gmail.com>
2026-01-04 18:31:41 -0300

    Atualizaçao Mauricio 04-01

--- NewCapit/dist/pages/Frm_AtualizaColetaMatriz.aspx.designer.cs
+++ NewCapit/dist/pages/Frm_AtualizaColetaMatriz.aspx.designer.cs
@@ -690,6 +690,15 @@ namespace NewCapit.dist.pages
         protected global::Subgurim.Controles.GMap GMap1;
 
         /// <summary>
+        /// Controle txtCarga.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCarga;
+
+        /// <summary>
         /// Controle rptColetas.
         /// </summary>
         /// <remarks>
